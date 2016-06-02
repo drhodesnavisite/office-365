21377c279625529cc9e438743a9735a0867a6e42
drhodesnavisite <drhodes@navisite.com>
2016-06-02 13:20:56 -0500

    Revert "Revert "Revert "Added scripts"""
    This reverts commit 73a6d3f0e629eb6ce56d247fe8e5fd95fa4dd4a5.

--- Create-MimecastConnectionFilter.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-HostedConnectionFilterPolicy > C:\$tenantDomain"_ConnectionFilter.txt"
-Enable-OrganizationCustomization
-Write-Host "Sleeping for 60 seconds..."
-Start-Sleep -Seconds 60
-New-HostedConnectionFilterPolicy -Name "Mimecast Inbound" -IPAllowList 207.211.31.0/25,207.211.30.0/24,205.139.110.0/24,205.139.111.0/24,216.205.24.0/24,63.128.21.0/24,205.217.25.135/32,205.217.25.132/32,207.211.41.113/32 -EnableSafeList $true
--- Verify-O365Domains.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Run this to complete domain verification
-
-Get-MsolDomain -TenantId $tenantId | Where-Object {$_.Status -eq "Unverified"} | ForEach-Object {Confirm-MsolDomain -domainname $_.name -TenantId $tenantId}
