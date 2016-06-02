4a8f65903b45e72292aef57ad54ef0315c1823ab
drhodesnavisite <drhodes@navisite.com>
2016-06-02 13:20:34 -0500

    Revert "Added scripts"
    This reverts commit 1fb8ee05c13a6fd2862519ac30a7d8b73de8d630.

--- Add-CompanyAdministratorRoleMember.ps1
+++
@@ -1,4 +0,0 @@
-﻿$roleGroupName = "Company Administrator"
-$roleMemberEmailAddress = Read-Host "Enter email address(es) to add to Company Administrator role:"
-
-Add-MsolRoleMember -RoleName $roleGroupName -RoleMemberEmailAddress $roleMemberEmailAddress -TenantId $tenantId
