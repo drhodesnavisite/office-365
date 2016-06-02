43a133330862131f976dcc51d8d46271a3c5936f
drhodesnavisite <drhodes@navisite.com>
2016-06-02 13:21:29 -0500

    Revert "Revert "Revert "Revert "Added scripts""""
    This reverts commit 21377c279625529cc9e438743a9735a0867a6e42.

---
+++ Add-CompanyAdministratorRoleMember.ps1
@@ -0,0 +1,4 @@
+﻿$roleGroupName = "Company Administrator"
+$roleMemberEmailAddress = Read-Host "Enter email address(es) to add to Company Administrator role:"
+
+Add-MsolRoleMember -RoleName $roleGroupName -RoleMemberEmailAddress $roleMemberEmailAddress -TenantId $tenantId
---
+++ Verify-O365Domains.ps1
@@ -0,0 +1,3 @@
+﻿#Run this to complete domain verification
+
+Get-MsolDomain -TenantId $tenantId | Where-Object {$_.Status -eq "Unverified"} | ForEach-Object {Confirm-MsolDomain -domainname $_.name -TenantId $tenantId}
